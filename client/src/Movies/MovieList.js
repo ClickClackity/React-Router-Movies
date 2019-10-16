7b829a68c24fc46560097a0210c078df07fbd818
Clackity <lockout18@yahoo.com>
2019-10-16 06:13:15 -0500

    Now officially done with all stretches

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -20,37 +21,13 @@ const MovieList = props => {
   }, []);
   
   return (
-    <button className="movie-list">
+    <div className="movie-list">
       {movies && movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </button>
-  );
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars, id } = movie;
-  return (
-  
-    <Link to={`/movies/${id}`}>
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars && stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
+        <Link key={movie.id} to={`movies/${movie.id}`} ><MovieCard key={movie.id} movie={movie} /></Link>
       ))}
     </div>
-    </Link>
   );
 }
+
 
 export default MovieList;
